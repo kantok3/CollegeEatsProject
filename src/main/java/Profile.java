83ac90d999db76a99289bc763d67877f31191887
kantok3 <91221303+kantok3@users.noreply.github.com>
2023-04-04 20:17:42 +0100

    Created profile class and added getters and setters

--- src/main/java/Profile.java
+++ src/main/java/Profile.java
@@ -1,3 +1,46 @@
 public class Profile(){
+    private String full_name;
+    private string email;
+    private char password;
+
+
+    public void getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        if (password.length() < 8 || !password.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("Password must be at least 8 characters long and contain at least one digit");
+        }
+        this.password = password;
+    }
+    public void getEmail(){
+        return this.password;
+    }
+    public void setEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        if (!email.matches(emailRegex)) {
+            throw new IllegalArgumentException("Invalid email address");
+        }
+        this.email = email;
+    }
+    public void getFull_Name(){
+        return this.fullName
+    }
+    public void setFull_Name(String fullName) {
+        if (!fullName.matches("^[A-Za-z]+\\s[A-Za-z]+$")) {
+            throw new IllegalArgumentException("Invalid full name");
+        }
+        this.full_name = fullName;
+    }
+    
+    
+    
+
+
+
+
+
+
 
 }
